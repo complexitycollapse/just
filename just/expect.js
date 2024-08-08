ba80efefa016dc4017a8185ce8ec102a9a9dde70
complexitycollapse <complexitycollapse@protonmail.com>
2024-08-08 13:38:50 +0100

    Refactor ExpectationFailure so all params are optional

--- just/expect.js
+++ just/expect.js
@@ -2,21 +2,21 @@ import { ExpectationFailure } from "./expectation-failure.js";
 
 export function expect(actual) {
   const stack = new Error().stack;
-  const loc = getLocation(stack);
+  const location = getLocation(stack);
 
   return {
 
     toBe(expected) {
       if (expected !== actual) {
-        throw new ExpectationFailure(loc, expected, actual);
+        throw new ExpectationFailure({location, expected, actual});
       }
     },
 
     toHaveLength(expected) {
       if (!Array.isArray(actual)) {
-        throw new ExpectationFailure(loc, "an Array", actual);
+        throw new ExpectationFailure({location, expected: "an Array", actual});
       } else if (actual.length !== expected) {
-        throw new ExpectationFailure(loc, "length to be " + expected, "length to be " + actual.length, {extraInfo: ["Array was:", actual]});
+        throw new ExpectationFailure({location, expected: "length to be " + expected, actual: "length to be " + actual.length, extraInfo: ["Array was:", actual]});
       }
     }
 
--- just/expectation-failure.js
+++ just/expectation-failure.js
@@ -1,17 +1,7 @@
 export class ExpectationFailure extends Error {
 
-  location;
-  expected;
-  actual;
-
-  constructor(location, expected, actual, extraParams) {
+  constructor(params) {
     super("Expectation failure");
-    this.location = location;
-    this.expected = expected;
-    this.actual = actual;
-    if (extraParams) {
-      if (Object.hasOwn(extraParams, "stack")) this.stack = extraParams.stack;
-      if (Object.hasOwn(extraParams, "extraInfo")) this.extraInfo = extraParams.extraInfo;
-    }
+    Object.assign(this, params);
   }
 }
