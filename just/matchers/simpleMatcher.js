67c2c126efb65c5ca601b1a08b0416df505f8b93
complexitycollapse <complexitycollapse@protonmail.com>
2024-08-08 19:45:54 +0100

    launch.json

--- just/matchers/simpleMatcher.js
+++ just/matchers/simpleMatcher.js
@@ -6,7 +6,6 @@ export function simpleMatcher(comparison, failString, showExpected = true, ignor
     if (!ignoreNot && not) {
       throw new ExpectationFailure({location, extraInfo: "can't use not with this matcher"});
     } else {
-      console.log("actual", actual, "expected", expected);
       if (!comparison(actual, expected)) {
         throw new ExpectationFailure({location, expected: showExpected ? [col.green(failString), expected] : col.green(failString), actual});
       }
