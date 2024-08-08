2f71889e0f67127f0d40c5d87ee9cc98add5a893
complexitycollapse <complexitycollapse@protonmail.com>
2024-08-08 01:21:36 +0100

    Console colours

---
+++ just/text-colours.js
@@ -0,0 +1,6 @@
+export const red = (str) => "\x1b[31m" + str + "\x1b[0m";
+export const redBack = (str) => "\x1b[90;41m" + str + "\x1b[0m";
+export const green = (str) => "\x1b[32m" + str + "\x1b[0m";
+export const greenBack = (str) => "\x1b[90;42m" + str + "\x1b[0m";
+export const yellow = (str) => "\x1b[93m" + str + "\x1b[0m";
+export const yellowBack = (str) => "\x1b[90;103m" + str + "\x1b[0m";
